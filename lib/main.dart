b97769b8a93c24b76426ccc2c8adde64298fe3e5
dan <eudangeld@gmail.com>
2020-08-31 20:30:31 -0300

    Creating app structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
 // Starting point
-// Ensure basic
 void main() {
-  print('I\'m a zombie');
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(App());
+  });
 }
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [],
+      child: MaterialApp(
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          appBarTheme: AppBarTheme(
+            brightness: Brightness.light,
+            elevation: 0,
+            color: Colors.white,
+          ),
+        ),
+        title: 'Primeira Mesa',
+        initialRoute: _initialRoute,
+        onGenerateRoute: useGenerateRoute,
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          AppLocalizationsDelegate(),
+        ],
+        supportedLocales: [
+          const Locale.fromSubtags(languageCode: 'pt', countryCode: 'BR'),
+        ],
+      ),
+    );
+  }
+}
+
+Provider<T> inProvider<T>(T value) =>
+    Provider<T>.value(value: value, updateShouldNotify: neverNotify);
+
+MaterialPageRoute inPageRoute(Widget child, [RouteSettings settings]) =>
+    MaterialPageRoute(builder: (context) => child, settings: settings);
