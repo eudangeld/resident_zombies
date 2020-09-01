e9431aac3d37f15870d8942a3b483f90ff0912c7
dan <eudangeld@gmail.com>
2020-08-31 21:00:08 -0300

    Init the login page class

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  static String get routeName => '@routes/login_page';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
