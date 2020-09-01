1fed94b8a3ed237998d6eec3711634b88d3b638e
dan <eudangeld@gmail.com>
2020-09-01 17:31:58 -0300

    feat: creating item data class

---
+++ lib/model/item_data.dart
@@ -0,0 +1,11 @@
+class ItemData {
+  final String location;
+  final int quantity;
+  final Map<String, dynamic> item;
+
+  ItemData({
+    this.location,
+    this.quantity,
+    this.item,
+  });
+}
