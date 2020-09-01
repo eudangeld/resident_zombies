06c52fdbe2cea5d64771760527ec6e2d848231f2
dan <eudangeld@gmail.com>
2020-09-01 05:53:08 -0300

    feat: Adding theme class

---
+++ lib/theme/global_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+LinearGradient zombieGradient = LinearGradient(
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter,
+  stops: [0.1, 0.5, 0.9],
+  colors: [Colors.lightGreen, Colors.green, Colors.greenAccent],
+);
