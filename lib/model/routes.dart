bdaa21405a8fc4df22b216ccdc25ae70daab8ff1
dan <eudangeld@gmail.com>
2020-08-31 21:01:26 -0300

    feat: adding a dedicated routes class to store routes and WidBuilder

---
+++ lib/model/routes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// Keep here routes that users need to be logged to aceess
+// This string are aceessed onApp ongenerateRoute method
+List<String> get useLoggedRoutes => [];
+
+// All unlogged routes
+Map<String, WidgetBuilder> get appRoutes => {};
