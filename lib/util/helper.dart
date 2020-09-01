17bb6f4e159c27d97866d3d759041c514a9d35f5
dan <eudangeld@gmail.com>
2020-08-31 21:13:08 -0300

    feat: Creating api class

---
+++ lib/util/helper.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+
+import '../locale/app_localizations.dart';
+
+/// A shortcut to avoid verbosity
+/// When need to acesses lz
+AppLocalizations lz(BuildContext context) => AppLocalizations.of(context);
