6e2f40ee8f1ff94ec47343b57d1ccdbf51dee03e
dan <eudangeld@gmail.com>
2020-09-03 18:59:04 -0300

    feat: stream and colors added - now is possbile see all operation value

--- lib/widgets/trade_itens_quantity.dart
+++ lib/widgets/trade_itens_quantity.dart
@@ -4,6 +4,7 @@ import 'package:resident_zombies/theme/global_theme.dart';
 import 'package:resident_zombies/util/helper.dart';
 import 'package:resident_zombies/widgets/bottom_sheet_button.dart';
 import 'package:resident_zombies/widgets/loading_widget.dart';
+import 'package:rxdart/subjects.dart';
 
 class TradeDetailsPage extends StatelessWidget {
   static String get routeName => '@routes/itens_quantity';
@@ -12,7 +13,7 @@ class TradeDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         bottomSheet: BottomSheetButton(
-          label: 'Adicionar',
+          label: 'Continuar',
           onPressed: () => print('adicionando item na sacola'),
         ),
         appBar: AppBar(
@@ -70,30 +71,90 @@ class TradeDetailsPage extends StatelessWidget {
                 ? 'Você está sem nenhum ítem, melhor você fazer alguma coisa antes que seja tarde!'
                 : 'Sobrevivente sem nenhum ítem, não vai durar muito tempo até ser infectado';
 
-            return _data.isEmpty
-                ? Padding(
-                    padding: const EdgeInsets.only(
-                        left: 15, right: 15, top: 50, bottom: 30),
-                    child: Text(
-                      _emptyMessage,
-                      textAlign: TextAlign.center,
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                  )
-                : Column(
-                    children: _data
-                        .map((item) => ItensStepper(
-                            iconPath: item['item']['name'],
-                            currentItem: ItemDetail(
-                                points: item['item']['points'],
-                                name: item['item']['name'],
-                                maxAvaliables: item['quantity'],
-                                units: 0)))
-                        .toList(),
-                  );
+            if (_data.isEmpty) {
+              return Padding(
+                padding: const EdgeInsets.only(
+                    left: 15, right: 15, top: 50, bottom: 30),
+                child: Text(
+                  _emptyMessage,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              );
+            }
+            final _steppers = _data.map((item) {
+              final BehaviorSubject<int> _bhsv = BehaviorSubject<int>.seeded(0);
+              return ItensStepper(
+                  streamTotal: _bhsv,
+                  iconPath: item['item']['name'],
+                  baseColor: _playerItens ? Colors.red : Colors.green,
+                  currentItem: ItemDetail(
+                      points: item['item']['points'],
+                      name: item['item']['name'],
+                      maxAvaliables: item['quantity'],
+                      units: 0));
+            }).toList();
+
+            return Column(
+                children: []
+                  ..add(TotalPOintsIndicator(
+                      baseColor: _playerItens ? Colors.red : Colors.green,
+                      steppers: _steppers))
+                  ..addAll(_steppers));
           }),
     );
+  }
+}
+
+// This class get the total point involved in the current transaction
+// ignore: must_be_immutable
+class TotalPOintsIndicator extends StatefulWidget {
+  final List<ItensStepper> steppers;
+  final Color baseColor;
+
+  const TotalPOintsIndicator(
+      {Key key, @required this.steppers, @required this.baseColor})
+      : super(key: key);
+
+  @override
+  _TotalPOintsIndicatorState createState() => _TotalPOintsIndicatorState();
+}
+
+class _TotalPOintsIndicatorState extends State<TotalPOintsIndicator> {
+  @override
+  void initState() {
+    super.initState();
+    widget.steppers.forEach((element) {
+      element.streamTotal.listen((event) {
+        total = 0;
+        widget.steppers.forEach((e) {
+          setState(() {
+            total += e.streamTotal.value;
+          });
+        });
+      });
+    });
+  }
+
+  int total = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Padding(
+      padding: const EdgeInsets.only(bottom: 30, top: 15),
+      child: Column(
+        children: <Widget>[
+          Text('TOTAL',
+              style: TextStyle(
+                  color: widget.baseColor,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold)),
+          Text(total.toString(),
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+        ],
+      ),
+    ));
   }
 }
 
@@ -101,8 +162,17 @@ class TradeDetailsPage extends StatelessWidget {
 class ItensStepper extends StatefulWidget {
   ItemDetail currentItem;
   final String iconPath;
+  final Color baseColor;
 
-  ItensStepper({Key key, @required this.currentItem, @required this.iconPath})
+  // ignore: close_sinks
+  final BehaviorSubject<int> streamTotal;
+
+  ItensStepper(
+      {Key key,
+      @required this.currentItem,
+      @required this.iconPath,
+      @required this.streamTotal,
+      @required this.baseColor})
       : super(key: key);
 
   @override
@@ -116,15 +186,17 @@ class _ItensStepperState extends State<ItensStepper> {
     } else {
       item.units++;
     }
+    widget.streamTotal.add(item.units * item.points);
     setState(() => widget.currentItem = item);
   }
 
   void decrementValues(ItemDetail item, BuildContext context) {
-    if (item.units - 1 <= 0) {
+    if (item.units - 1 < 0) {
       item.units = item.maxAvaliables;
     } else {
       item.units--;
     }
+    widget.streamTotal.add(item.units * item.points);
     setState(() => widget.currentItem = item);
   }
 
@@ -134,15 +206,18 @@ class _ItensStepperState extends State<ItensStepper> {
       children: <Widget>[
         Text(
           '${widget.currentItem.name}',
-          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black87),
+          style:
+              TextStyle(fontWeight: FontWeight.bold, color: widget.baseColor),
         ),
         Row(
           children: <Widget>[
-            // Container(height: 50, width: 50, child: Image.asset(iconPath)),
             IconButton(
               padding: const EdgeInsets.all(0.0),
               onPressed: () => decrementValues(widget.currentItem, context),
-              icon: Icon(Icons.remove),
+              icon: Icon(
+                Icons.remove,
+                color: widget.baseColor,
+              ),
             ),
             Expanded(
               child: Padding(
@@ -154,7 +229,7 @@ class _ItensStepperState extends State<ItensStepper> {
                     Text(
                       widget.currentItem.units.toString(),
                       style: TextStyle(
-                        color: heavyDark,
+                        color: widget.baseColor,
                         fontSize: 28,
                         fontWeight: FontWeight.bold,
                       ),
@@ -166,7 +241,10 @@ class _ItensStepperState extends State<ItensStepper> {
             IconButton(
               padding: const EdgeInsets.all(0.0),
               onPressed: () => incrementunits(widget.currentItem, context),
-              icon: Icon(Icons.add),
+              icon: Icon(
+                Icons.add,
+                color: widget.baseColor,
+              ),
             ),
           ],
         )
