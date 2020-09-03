d0b52ef2001a9699b0b0de18d5da05b4812be7e7
dan <eudangeld@gmail.com>
2020-09-03 07:49:20 -0300

    feat: updating appstate with comments

--- lib/model/app_state.dart
+++ lib/model/app_state.dart
@@ -3,13 +3,21 @@ import 'package:resident_zombies/model/user.dart';
 import 'package:rxdart/subjects.dart';
 
 class AppState {
-  final currentMapPosition = BehaviorSubject<LatLng>.seeded(LatLng(0, 0));
-  final user = BehaviorSubject<User>.seeded(User());
-
   /// Default app location
   ///
   /// [Codeminer42 SP location]
   /// Used when geolocator plugin throws an exception
   /// or u need some location and dant want to think wich location use
   final codeminerLocation = LatLng(-23.623660, -46.695790);
+
+  /// Use that value to show position on maps
+  /// Deafults to [Codeminer42 SP location]
+  // ignore: close_sinks
+  final currentMapPosition =
+      BehaviorSubject<LatLng>.seeded(LatLng(-23.623660, -46.695790));
+
+  ///current [User] on state
+  ///last registered
+  // ignore: close_sinks
+  final user = BehaviorSubject<User>.seeded(User());
 }
