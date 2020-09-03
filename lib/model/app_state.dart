f545238e041ce1e6fffa94d8c7c4a9e0721f5da6
dan <eudangeld@gmail.com>
2020-09-03 17:31:16 -0300

    adding trade itens on state

--- lib/model/app_state.dart
+++ lib/model/app_state.dart
@@ -2,6 +2,8 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:resident_zombies/model/user.dart';
 import 'package:rxdart/subjects.dart';
 
+import 'trade_item_details.dart';
+
 class AppState {
   /// Default app location
   ///
@@ -20,4 +22,31 @@ class AppState {
   ///last registered
   // ignore: close_sinks
   final user = BehaviorSubject<User>.seeded(User());
+
+  /// Indicate other player [id]
+  ///
+  /// used to retrieve and send itens between player and this survivor
+  String traderId;
+
+  /// Used to indicate icons path on assets folder
+  ///
+  /// TODO: Refactory when possible
+  final assetsMap = {
+    'Fiji Water': 'assets/itens/water.png',
+    'First Aid Pouch': 'assets/itens/aid.png',
+    'AK47': 'assets/itens/ak.png',
+    'Campbell Soup': 'assets/itens/soup.png',
+  };
+
+  ///Player itens
+  ///
+  ///
+  /// used to pay trade
+  final currentPlayerItens = BehaviorSubject<List<ItemDetail>>.seeded(null);
+
+  ///Trader survivor itens
+  ///
+  ///
+  /// indicate other players itens to trade
+  final otherPlayerItens = BehaviorSubject<List<ItemDetail>>.seeded(null);
 }
